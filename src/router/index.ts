6f1dd689f6f28d47599b63538da9a53e6d39646b
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-11-09 11:45:30 +0900

    set title and description

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import { getCurrentUser } from "../settings/firebase";
+import { setTitle, setDescription } from "@/composables/set-title-description";
 import Home from "../views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
@@ -7,6 +8,7 @@ const routes: Array<RouteRecordRaw> = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: { title: "Home", desc: "たこの世話管理ボード" },
   },
   {
     path: "/about",
@@ -16,17 +18,19 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: { title: "About", desc: "説明ページ" },
   },
   {
     path: "/login",
     name: "Login",
     component: () => import("../views/Login.vue"),
+    meta: { title: "Login", desc: "ログインページ" },
   },
   {
     path: "/records",
     name: "Records",
     component: () => import("../views/Records.vue"),
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, title: "Records", desc: "管理ボード" },
   },
 ];
 
@@ -38,15 +42,13 @@ const router = createRouter({
 router.beforeEach(async (to, from, next) => {
   const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
   const currentUser = await getCurrentUser();
-  if (requiresAuth) {
-    if (!currentUser) {
-      next({
-        path: "/login",
-        query: { redirect: to.fullPath },
-      });
-    } else {
-      next();
-    }
+  setTitle(to.meta.title as string);
+  setDescription(to.meta.desc as string);
+  if (requiresAuth && !currentUser) {
+    next({
+      path: "/login",
+      query: { redirect: to.fullPath },
+    });
   } else {
     next();
   }
