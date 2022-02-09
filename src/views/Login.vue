e66e63f3fcfc5f4f8b685489e55268ef5c5a44af
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-02-09 17:38:06 +0900

    change the page on which username is displayed

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,6 +15,9 @@
     <div v-else>
       <button @click="logout">Logout</button>
     </div>
+    <div v-if="state.loggedIn" class="login-name-display">
+      {{ state.loginUserName }}としてログインしています。
+    </div>
     <div v-if="state.errorMessage" class="error-msg">
       {{ state.errorMessage }}
     </div>
@@ -22,16 +25,18 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "vue";
+import { defineComponent, reactive, onMounted } from "vue";
 import { useRouter, useRoute } from "vue-router";
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "@/settings/firebase";
+import getUserName from "@/composables/get-username";
 
 interface State {
   userEmail: string;
   password: string;
   errorMessage: string;
   loggedIn: boolean;
+  loginUserName: string;
 }
 
 export default defineComponent({
@@ -41,9 +46,18 @@ export default defineComponent({
       password: "",
       errorMessage: "",
       loggedIn: false,
+      loginUserName: "",
     });
     const router = useRouter();
     const route = useRoute();
+
+    onMounted(async () => {
+      const userName = await getUserName();
+      state.loggedIn = !!userName;
+      if (userName !== null) {
+        state.loginUserName = userName;
+      }
+    });
 
     const login = () => {
       // eslint-disable-next-line no-console
@@ -85,3 +99,10 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.login-name-display {
+  width: 80%;
+  margin: 16px auto;
+}
+</style>
