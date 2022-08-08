bae0519c16a69477c22121e69a542db3bc6f8969
Aiwaka <Kw.I.KU.1414@gmail.com>
2022-08-08 11:23:48 +0900

    enable to fold

--- src/components/QueryMaker.vue
+++ src/components/QueryMaker.vue
@@ -1,28 +1,39 @@
 <template>
   <!-- Firestore検索クエリを作成し取得ボタンを押したらコールバックに渡す -->
   <div class="query-maker">
-    <h3>検索条件</h3>
-    <date-selector-vue
-      v-on:end-date-changed="endDateChanged"
-      v-on:start-date-changed="startDateChanged"
-    />
-    タイプ指定：
-    <select
-      name="query-record-type"
-      v-model="recordType"
-      v-on:change="recordTypeChanged"
-    >
-      <option value="-1">---</option>
-      <option
-        v-for="(type, index) in recordTypeStr"
-        :key="type"
-        :value="'' + index"
-      >
-        <!-- valueをバインドする際は-1に合わせるためstringに変換している. -->
-        {{ type }}
-      </option>
-    </select>
-    <button @click="fetch" :disabled="fetchButtonDisabled">取得</button>
+    <template v-if="!boxOpening">
+      <div class="query-maker__toggle-button" @click="toggleBoxOpening">+</div>
+      <span>検索ボックス</span>
+    </template>
+    <template v-else>
+      <div class="query-maker__toggle-button" @click="toggleBoxOpening">-</div>
+      <div class="query-maker-input-box">
+        <h4>期間指定</h4>
+        <date-selector-vue
+          v-on:end-date-changed="endDateChanged"
+          v-on:start-date-changed="startDateChanged"
+        />
+        <div>
+          <h4>タイプ指定</h4>
+          <select
+            name="query-record-type"
+            v-model="recordType"
+            v-on:change="recordTypeChanged"
+          >
+            <option value="-1">---</option>
+            <option
+              v-for="(type, index) in recordTypeStr"
+              :key="type"
+              :value="'' + index"
+            >
+              <!-- valueをバインドする際は-1に合わせるためstringに変換している. -->
+              {{ type }}
+            </option>
+          </select>
+        </div>
+        <button @click="fetch" :disabled="fetchButtonDisabled">取得</button>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -45,6 +56,7 @@ const tempToday = new Date();
 const prevWeekDay = new Date(tempToday.setDate(tempToday.getDate() - 7));
 
 interface State {
+  boxOpening: boolean;
   endDate: string;
   fetchButtonDisabled: boolean;
   recordType: string;
@@ -62,15 +74,21 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const { endDate, fetchButtonDisabled, startDate, recordType } = toRefs(
-      reactive<State>({
-        endDate: toDateString(today),
-        fetchButtonDisabled: true,
-        recordType: "-1",
-        startDate: toDateString(prevWeekDay),
-      })
-    );
+    const { boxOpening, endDate, fetchButtonDisabled, startDate, recordType } =
+      toRefs(
+        reactive<State>({
+          boxOpening: false,
+          endDate: toDateString(today),
+          fetchButtonDisabled: true,
+          recordType: "-1",
+          startDate: toDateString(prevWeekDay),
+        })
+      );
     onMounted(() => fetch());
+
+    const toggleBoxOpening = () => {
+      boxOpening.value = !boxOpening.value;
+    };
 
     const fetch = async () => {
       // 取得時期期間, 検索条件等を総合したクエリを作成し, propsで受け取っているコールバックに渡しながら実行
@@ -101,6 +119,7 @@ export default defineComponent({
     };
 
     return {
+      boxOpening,
       endDate,
       endDateChanged,
       fetch,
@@ -110,6 +129,7 @@ export default defineComponent({
       recordTypeStr,
       startDate,
       startDateChanged,
+      toggleBoxOpening,
     };
   },
 });
@@ -117,7 +137,19 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 .query-maker {
-  border: 1px solid #777;
+  border: 1px dashed #777777;
+  padding: 1rem 0.6rem;
+  &__toggle-button {
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    line-height: 1rem;
+    border: 1px solid #000000;
+    cursor: pointer;
+  }
+  span {
+    margin: 0 0.5rem;
+  }
 
   h3 {
     margin: auto;
